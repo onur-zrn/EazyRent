4e9a951439f3b57bafd038b2acd260d9e9c10206
Onur Zeren <onurzrn@gmail.com>
2025-06-22 13:18:23 +0300

    Log düzenlemesi yapıldı

--- src/main/java/com/eazyrent/Main.java
+++ src/main/java/com/eazyrent/Main.java
@@ -3,8 +3,13 @@ package com.eazyrent;
 import com.eazyrent.controller.MainController;
 import com.eazyrent.utility.JPAUtility;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
     public static void main(String[] args) {
+        Logger.getLogger("org.hibernate").setLevel(Level.SEVERE); // Sadece ERROR ve SEVERE mesajları gösterilir
+        Logger.getLogger("org.hibernate.SQL").setLevel(Level.OFF); // SQL sorguları tamamen kapatılır
         MainController mainController = new MainController();
         try {
             mainController.run();
